742633c2d6539a0150e9ea00767c98a847e7067a
LAAGUIDI <laaguidi.cherif90@gmail.com>
2019-04-09 15:22:47 +0200

    loginEroor

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,9 @@
 
-import {Component, OnInit} from '@angular/core';
-import {PATH_HOME, PATH_WELCOME} from '../app.routes.constante';
-import {Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { PATH_HOME, PATH_WELCOME, PATH_LOGIN } from '../app.routes.constante';
+import { Router } from '@angular/router';
+import { UserService } from '../user/user.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +11,15 @@ import {Router} from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor(private router: Router) {
+  errorMessage: string;
+  loginForm: FormGroup;
+  constructor(private userService: UserService, private formBuilder: FormBuilder, private router: Router) {
+    this.loginForm = formBuilder.group({
+      login: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
+
   title = 'Private Showcase';
   isHidden = true;
 
@@ -20,14 +28,50 @@ export class LoginComponent implements OnInit {
   }
 
   goToHomePage() {
-    this.router.navigate([PATH_HOME]);
+
   }
 
   hidePassword() {
     this.isHidden = !this.isHidden;
   }
 
-  ngOnInit() {
+ 
+
+
+
+  get login() {
+    return this.loginForm.get('login');
   }
 
-}
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+
+  ngOnInit() {
+    
+  }
+
+  userLogin() {
+    this.errorMessage = "";
+    if (this.loginForm.invalid) {
+      this.errorMessage = "EMail and / or password is incorrect";
+      return;
+    }
+    this.userService.login(this.login.value, this.password.value)
+      .then(data => {
+        localStorage.setItem('currentUser', JSON.stringify(data));
+        this.router.navigate( [PATH_HOME]);
+      }, error => {
+        if(error.status === 404) {
+          this.errorMessage = "No user was found with the following Email/Password";
+        }
+        if(error.status === 400) {
+          this.errorMessage = "EMail and / or password is incorrect";
+        }
+      });
+  }
+
+  }
+
+ 
