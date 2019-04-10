6672a4cb77fed4bf048a0b7f14bdb697434db991
Cherif <32360655+m2cci-CLI@users.noreply.github.com>
2019-04-10 11:38:22 +0200

    Auto complition (#17)
    * auto-Complition des villes dans le formulaire regester
    
    * autoCompletion de la ville dans le formulaire

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 import {PATH_LOGIN, PATH_WELCOME} from '../app.routes.constante';
 import {Router} from '@angular/router';
-
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
@@ -15,18 +16,18 @@ export class RegisterComponent implements OnInit {
   email = new FormControl('', [Validators.required, Validators.email]);
   title = 'Private Showcase';
   checked = false;
+  myControl = new FormControl();
+  options: string[] = ['Lyon', 'Marseille – Aix-en-Provence', 'Toulouse', 'Bordeaux', 'Nice', 'Strasbourg', 'Rennes'];
+  filteredOptions: Observable<string[]>;
 
   constructor(private router: Router) {
   }
-
   hidePassword() {
     this.isHidden = !this.isHidden;
   }
-
   cancel() {
     this.router.navigate([PATH_WELCOME]);
   }
-
   goToLogPage() {
     this.router.navigate([PATH_LOGIN]);
   }
@@ -36,8 +37,15 @@ export class RegisterComponent implements OnInit {
       this.email.hasError('email') ? 'Not a valid email' :
         '';
   }
-
   ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
   }
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
 
+    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  }
 }
