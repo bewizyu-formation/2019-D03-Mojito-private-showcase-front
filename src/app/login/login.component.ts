038374de152cd59c544f9e59ae27d2be69b809ae
UlterPendragon <47243696+UlterPendragon@users.noreply.github.com>
2019-04-10 11:30:43 +0200

    Login form controland validators (#15)
    * commit first change to login Form with control and validators. 
    
    Password uppercase, lowercase, number is not working yet
    
    * commit some tweaking on mat-error for Form control/validators
    
    commit some tweaking on mat-error for Form control/validators
    Maj/min/num for password still doesn't work
    
    * envoi des changements mis en place pour faire le Form Control
    
    une base a été commencé
    ainsi que la mise en place de l'ensemble des validators

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,9 +2,8 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {PATH_HOME, PATH_WELCOME} from '../app.routes.constante';
 import {Router} from '@angular/router';
-import {UserRepository} from '../user/user.repository';
 import {UserService} from '../user/user.service';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,17 +11,36 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
   errorMessage: string;
+
   loginForm: FormGroup;
-  constructor(private userService: UserService, private formBuilder: FormBuilder, private router: Router) {
-    this.loginForm = formBuilder.group({
-      login: ['', Validators.required],
-      password: ['', Validators.required]
+  usernameCtrl: FormControl;
+  passwordCtrl: FormControl;
+  title = 'Private Showcase';
+  isHidden = true;
+
+
+
+ constructor(private userService: UserService, private fb: FormBuilder, private router: Router) {
+   this.usernameCtrl = fb.control('', [Validators.required]);
+   this.passwordCtrl = fb.control('', [
+     Validators.required,
+     Validators.minLength(8),
+     Validators.pattern('(?=.*\d)(?=.*[a-z])(?=.*[A-Z])' )
+   ]);
+   this.loginForm = fb.group({
+      username: this.usernameCtrl,
+      password: this.passwordCtrl
     });
   }
 
-  title = 'Private Showcase';
-  isHidden = true;
+
+
+  getErrorMessage() {
+    return this.password.hasError('required') ? 'Le mot de passe est obligatoire' :
+      '';
+  }
 
   cancel() {
     this.router.navigate([PATH_WELCOME]);
@@ -32,20 +50,22 @@ export class LoginComponent implements OnInit {
     this.isHidden = !this.isHidden;
   }
 
-  get login() {
-    return this.loginForm.get('login');
+  get username() {
+    return this.loginForm.get('username');
   }
 
   get password() {
     return this.loginForm.get('password');
   }
+
+
   userLogin() {
     this.errorMessage = '';
     if (this.loginForm.invalid) {
-      this.errorMessage = 'L\'email ou le mot de passe est invalide';
+      this.errorMessage = 'L\' identifiant ou le mot de passe est invalide';
       return;
     }
-    this.userService.login(this.login.value, this.password.value)
+    this.userService.login(this.username.value, this.password.value)
       .then(data => {
         localStorage.setItem('currentUser', JSON.stringify(data));
         this.router.navigate( [PATH_HOME]);
@@ -54,7 +74,7 @@ export class LoginComponent implements OnInit {
           this.errorMessage = 'Utilisateur inexistant';
         }
         if (error.status === 400) {
-          this.errorMessage = 'L\'email ou le mot de passe est invalide';
+          this.errorMessage = 'L\' identifiant ou le mot de passe est invalide';
         }
       });
   }
